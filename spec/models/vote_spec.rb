f2debae695399066710a576ca245822e297b727c
vlaew <vlaew5@gmail.com>
2016-01-13 01:13:33 +0300

    Add missing vote validation tests

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -27,5 +27,35 @@ describe Vote, type: :model do
         expect(@vote.errors.any?).to eq(false)
       end
     end
+
+    context 'when has no user' do
+      before do
+        @vote = build(:vote, user: nil, tweet: @tweet)
+        @vote.valid?
+      end
+
+      it 'should be invalid' do
+        expect(@vote.valid?).to eq(false)
+      end
+
+      it 'should contain error for user' do
+        expect(@vote.errors[:user].any?).to eq(true)
+      end
+    end
+
+    context 'when has no tweet' do
+      before do
+        @vote = build(:vote, user: @user, tweet: nil)
+        @vote.valid?
+      end
+
+      it 'should be invalid' do
+        expect(@vote.valid?).to eq(false)
+      end
+
+      it 'should contain error for tweet' do
+        expect(@vote.errors[:tweet].any?).to eq(true)
+      end
+    end
   end
 end
