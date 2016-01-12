14a588495979bd75445e1146efc362a9477f5c25
vlaew <vlaew5@gmail.com>
2016-01-12 22:43:13 +0300

    Postgres & RSpec
    Set up Postgres
    Set up RSpec

---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,28 @@
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:all) do
+    DatabaseCleaner.start
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+  config.after(:all) do
+    DatabaseCleaner.clean
+  end
+end
